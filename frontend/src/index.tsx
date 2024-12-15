bb71602b46de1a9b31e0992fdeb065fed48000d7
tomoya <arata5.18830@gmail.com>
2024-12-15 17:40:58 +0900

    createContext使用

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import { UserProvider } from './components/UserContext'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+]);
 
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container!);
-root.render(<App />);
-
+root.render(
+    <StrictMode>
+        {/* <UserProvider>            */}
+            <RouterProvider router= {router} />     
+        {/* </UserProvider> */}
+    </StrictMode>
+);
